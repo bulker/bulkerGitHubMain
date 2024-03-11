19ca4e4abaaff6b118a318af9d4808412ddd22ff
bulker <bulker@users.noreply.github.com>
2024-03-11 23:13:51 +0900

    Create dateutil.js

---
+++ js/dateutil.js
@@ -0,0 +1,7 @@
+// 날짜를 YYYY-MM-DD 형식으로 포맷하는 함수
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
