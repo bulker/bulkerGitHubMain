89f11c22d4f0f5d974ef049a941d983d701f497e
bulker <bulker@users.noreply.github.com>
2024-03-24 00:20:19 +0900

    로그보완

--- js/browserenv.js
+++ js/browserenv.js
@@ -6,11 +6,11 @@ function PrintBrowserEnv() {
     console.log('PrintBrowserEnv')
     console.log('PrintBrowserEnv')
 
-    console.log(document.URL);
-    console.log(location.href); // "https://www.example.com/"
-    console.log(window.location.href); // "https://www.example.com/"
-    console.log(location.search); // "?param1=value1&param2=value2"
-    console.log(location.hash); // "#section1"
+    console.log('document.URL',document.URL);
+    console.log('location.href',location.href); // "https://www.example.com/"
+    console.log('window.location.href',window.location.href); // "https://www.example.com/"
+    console.log('location.search',location.search); // "?param1=value1&param2=value2"
+    console.log('location.hash',location.hash); // "#section1"
    
     //https://developer.mozilla.org/ko/docs/Web/API/Document/URL
     //https://developer.mozilla.org/ko/docs/Web/API/Location    
